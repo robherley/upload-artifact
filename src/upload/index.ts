d6f20b116bac38fdebf9925ba4a3e27189ccef7f
Rob Herley <robherley@github.com>
2024-04-29 14:00:44 -0400

    debug it a bit

--- src/upload/index.ts
+++ src/upload/index.ts
@@ -1,6 +1,19 @@
+import log from 'why-is-node-running'
 import * as core from '@actions/core'
 import {run} from './upload-artifact'
 
-run().catch(error => {
-  core.setFailed((error as Error).message)
-})
+const main = async () => {
+  const interval = setInterval(() => {
+    log()
+  }, 5000)
+
+  try {
+    await run()
+  } catch (error) {
+    core.setFailed((error as Error).message)
+  } finally {
+    clearInterval(interval)
+  }
+}
+
+main()
